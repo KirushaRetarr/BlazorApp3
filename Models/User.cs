a3979729853a9910a2431195cf868b84c685a360
Kirill Durnin <kirill2308@mail.ru>
2025-03-15 22:34:54 +0500

    Добавьте файлы проекта.

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp3.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        public string Name { get; set; }
+
+        [Range(1, 120, ErrorMessage = "Возраст должен быть от 1 до 120")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Введите Email")]
+        [EmailAddress(ErrorMessage = "Неверный формат Email")]
+        public string Email { get; set; }
+    }
+}
